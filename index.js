20a51190ceb9257548873b61e82d7e0f6259a23d
fatemzh <abid.fatem@hotmail.fr>
2023-10-09 18:44:18 +0200

    X

---
+++ index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const navbar = (
+    <nav>
+        <h1>Fatem's Shop</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(
+    navbar,
+    document.getElementById("root")
+)
