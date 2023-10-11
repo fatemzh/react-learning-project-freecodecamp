984efe3a0042b65d9b2d96b90cc01a1778355777
fatemzh <abid.fatem@hotmail.fr>
2023-10-11 19:45:39 +0200

    x

--- SecondProject/index.js
+++ SecondProject/index.js
@@ -3,12 +3,44 @@ const {React, ReactDOM} = window;
 function Page (){
     return (
         <div>
+            <Header/>
+            <MainContent />
+            <Footer/>
+        </div>
+    )
+}
+
+function Header (){
+    return (
+        <header>
+            <nav className="nav">
+                <img src="../SecondProject/logo.png" alt="" className="nav-logo"/>
+                <ul className="nav-items">
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+function MainContent (){
+    return(
+        <div className="main-content">
             <h1>Reasons why I want to learn React</h1>
-            <ol>
+            <ol className="list-reasons">
                 <li>Find a job</li>
                 <li>Improve my coding skills</li>
             </ol>
         </div>
+    )
+}
+function Footer (){
+    return(
+        <footer className="footer">
+            <small>@2023 ABID development. All rights reserved.</small>
+        </footer>
     )
 }
 
