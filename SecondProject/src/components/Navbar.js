8fd3244c3efcfdd773be19cfcc05f3b85e32da7b
fatemzh <abid.fatem@hotmail.fr>
2023-11-11 14:55:39 +0100

    React Site css done

--- SecondProject/src/components/Navbar.js
+++ SecondProject/src/components/Navbar.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import '../styles/index.css'
+import logo from '../images/logo.png'
 
 export default function Navbar(){
     return (
-        <h1>Navigation bar</h1>
+        <nav className="navbar">
+            <div className='logo-title'>
+                <img src={logo} alt="" />
+                <h2>ReactFacts</h2>
+            </div>
+            <div className='onglets'>
+                <h3>React Course -Project 1</h3>
+            </div>
+        </nav>
     )
 }
 
